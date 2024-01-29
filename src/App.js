0dcfaccb2eab82b939d8c80636427d12a23a36b0
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-01-28 23:47:30 -0300

    accesibilidad

--- src/App.js
+++ src/App.js
@@ -69,12 +69,11 @@ export default function App() {
               </a>
             ))}
           </div>
-          <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+          <dl className="info mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
             {stats.map((stat) => (
               <div key={stat.name} className="flex flex-col-reverse">
                 <dt className="textos text-base leading-7 text-gray-100">{stat.name}</dt>
                 <dd className="textos text-3xl leading-9 tracking-tight text-white">{stat.value}</dd>
-                
               </div>
             ))}
           </dl>
