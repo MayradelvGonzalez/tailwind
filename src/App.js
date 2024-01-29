bd3eb30971b94d07f1ee35481e5627bdad46932e
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-01-28 21:16:22 -0300

    elementos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './App.css';
+import portadaPortfolio from './assets/portadaPortfolio.webp';
 
 const links = [
   { name: 'Linkedin', href: 'https://www.linkedin.com/in/mayra-gonz%C3%A1lez-v/'},
@@ -13,13 +15,19 @@ const stats = [
   
 ]
 
-export default function Example() {
+export default function App() {
+  // const imagenEstilo = {
+  // }
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-52">
+    <div className="fondo relative isolate overflow-hidden bg-gray-900 py-24 sm:py-52">
       <img
-        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-        alt=""
+      src={portadaPortfolio}
+  
+        // src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+        alt="imagen de fondo de pagina"
+        aria-label='fondo de pagina'
         className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+        // style={imagenEstilo}
       />
       <div
         className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
