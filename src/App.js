939d7d494b9f7ed2ca4c95f48ef3034b2c4b6190
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-01-28 23:25:13 -0300

    fondoContraste

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const links = [
   
 ]
 const stats = [
-  { name: 'Aqui va un breve about me, mas o menos como lo del cv', value: 'More Info', className: 'texto'},
+  { name: 'Aqui va un breve about me, mas o menos como lo del cv', value: 'More Info'},
   { name: 'Aqui va una breve je informacion sobre miu experiencia', value: 'Experience' },
   { name: 'Aqui iria que es lo que estoy estudiando o haciendo ahora', value: 'Intereses' },
   
@@ -23,7 +23,7 @@ export default function App() {
      
       <img
       src={portadaPortfolio}
-  id='fondo'
+
         // src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
         alt="imagen de fondo de pagina"
         aria-label='fondo de pagina'
@@ -56,8 +56,8 @@ export default function App() {
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font tracking-tight text-white sm:text-6xl">Mayra del Valle González</h2>
-          <p className="mt-20 text-4xl leading-9 text-gray-100">
+          <h2 className=" textos text-4xl font tracking-tight text-white sm:text-6xl">Mayra del Valle González</h2>
+          <p className=" textos mt-20 text-4xl leading-9 text-gray-100">
         Full-Stack developer
           </p>
         </div>
@@ -72,8 +72,8 @@ export default function App() {
           <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
             {stats.map((stat) => (
               <div key={stat.name} className="flex flex-col-reverse">
-                <dt className="texto text-base leading-7 text-gray-100">{stat.name}</dt>
-                <dd className="text-3xl leading-9 tracking-tight text-white">{stat.value}</dd>
+                <dt className="textos text-base leading-7 text-gray-100">{stat.name}</dt>
+                <dd className="textos text-3xl leading-9 tracking-tight text-white">{stat.value}</dd>
                 
               </div>
             ))}
