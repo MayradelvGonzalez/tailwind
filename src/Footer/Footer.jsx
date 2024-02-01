cd187dac73a948b4a5c8200f390817c974cc34c5
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-01-31 22:27:19 -0300

    correcciones

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,9 +1,8 @@
 import './Footer.css';
-import LogoNsf  from '../assets/LogoNsf.png';
 function Footer(){
     return(
         <div className='foot'> <h3>Portfolio Mayra 💖 2024</h3>
-        <div className='logo'><img src={LogoNsf} alt="Logo de página" /></div>
+     
        </div>
     )
 }
