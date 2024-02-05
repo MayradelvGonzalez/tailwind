727a38d6580037d586df844be0a81277e5fbc959
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-02-05 19:46:34 -0300

    mediaQ

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ const links = [
 
 ]
 const stats = [
-  { name: 'Aqui va un breve about me, mas o menos como lo del cv', value: 'More Info'},
-  { name: 'Aqui va una breve je informacion sobre miu experiencia', value: 'Experience'},
-  { name: 'Aqui iria que es lo que estoy estudiando o haciendo ahora', value: 'Intereses'},
+  { name: 'Disfruto aprender, resolver los problemas y crear cosas nuevas', value: 'About Me'},
+  { name: 'Soy docente de música, egresada un bootcamp de desarrollo FullStack MERN, tutora en un curso de programacion backend y autodidacta', value: 'Experiencia'},
+  { name: 'Me interesa adquirir nuevas habilidades tanto tecnicas como blandas. Entre mis intereses estan la accesibilidad web, algo que he aplicado este en este portfolio. Creo que como programadores debemos crear herramientas que lleguen al alcance de la mayor cantidad de personas mas alla de sus habilidades.', value: 'Intereses'},
 
 ]
 
@@ -21,7 +21,7 @@ export default function App() {
 
   return (
     <div className="fondo relative isolate overflow-hidden bg-gray-900 py-24 sm:py-52">
-     
+              <div className='logo'><img src={LogoNsf} alt="Logo de página"  aria-label='logo de pagina' /></div>
       <img src={portadaPortfolio}
         // src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
         alt="imagen de fondo de pagina"
@@ -45,6 +45,7 @@ export default function App() {
         className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
         aria-hidden="true"
       >
+
         <div
           className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
           style={{
@@ -56,10 +57,10 @@ export default function App() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
         <div className="tituloLogo">
-          <h2 className=" textos tit text-4xl font tracking-tight text-white sm:text-6xl">
+          <h2 className=" textos tit text-4xl font tracking-tight text-white sm:text-6xl" id='nombre'>
             Mayra del Valle González</h2>
-            <div className='logo'><img src={LogoNsf} alt="Logo de página"  aria-label='logo de pagina' /></div></div>
-          <p className=" textos mt-20 text-4xl leading-9 text-gray-100">
+           </div>
+          <p className="textos mt-20 text-4xl leading-9 text-gray-100" id='subtitulo'>
             Full-Stack developer</p>
         </div>
         <div className="mx-auto mt-20 max-w-2xl lg:mx-0 lg:max-w-none">
@@ -70,11 +71,11 @@ export default function App() {
               </a>
             ))}
           </div>
-          <dl className="info mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+          <dl className="info mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4" id='cuadro'>
             {stats.map((stat, index) => (
               <div key={stat.name} className={`flex flex-col-reverse stat${index + 1}`}>
-                <dt className="textos text-base leading-8 text-gray-100">{stat.name}</dt>
-                <dd className="textos infor text-3xl leading-9 tracking-tight text-white">{stat.value}</dd>
+                <dt className="textos text-base leading-8 text-gray-100" id='info'>{stat.name}</dt>
+                <dd className="textos infor text-3xl leading-9 tracking-tight text-white" id='titInfo'>{stat.value}</dd>
               </div>
             ))}
           </dl>
