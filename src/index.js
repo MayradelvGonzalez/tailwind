5b2196c3d1a0b651bbda2e20c7fe18b966dd7124
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-02-05 23:42:52 -0300

    ubicando Componente

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import Footer from './Footer/Footer';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
