bb17a7e25ae6bea731017e9aff2f9f63eed9543f
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-02-05 23:43:37 -0300

    configTailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/App.js"
-  ],
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/App.js"
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+});
