e2e9a85bf26c9fa26ead381a145e6494698f57d6
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-02-13 20:48:52 -0300

    arreglandoDesplegavles

--- src/App.js
+++ src/App.js
@@ -8,34 +8,27 @@ const links = [
   { name: 'Linkedin', href: 'https://www.linkedin.com/in/mayra-gonz%C3%A1lez-v/' },
   { name: 'Github', href: 'https://github.com/MayradelvGonzalez' },
   { name: 'Email', href: "mailto:mayradelvallegonzalez@gmail.com" },
+];
 
-]
 const stats = [
   { name: 'About Me', value: 'Disfruto aprender, resolver los problemas y crear cosas nuevas' },
   { name: 'Experience', value: 'Soy docente de música, egresada un bootcamp de desarrollo FullStack MERN, tutora en un curso de programacion backend y autodidacta' },
-  { name: 'Intereses', value: 'Me interesa adquirir nuevas habilidades tanto tecnicas como blandas. Entre mis intereses estan la accesibilidad web, algo que he aplicado este en este portfolio. Creo que como programadores debemos crear herramientas que lleguen al alcance de la mayor cantidad de personas mas alla de sus habilidades' },
-
-]
+  { name: 'Interests', value: 'Me interesa adquirir nuevas habilidades tanto tecnicas como blandas. Entre mis intereses estan la accesibilidad web, algo que he aplicado este en este portfolio. Creo que como programadores debemos crear herramientas que lleguen al alcance de la mayor cantidad de personas mas alla de sus habilidades' },
+];
 
 export default function App() {
-  const [buttonStates, setButtonStates] = useState({
-    button1: false,
-    button2: false,
-    button3: false,
-  });
+  const [selectedOption, setSelectedOption] = useState(null);
 
   const handleButtonClick = (buttonName) => {
-    setButtonStates((prevState) => ({
-      ...prevState,
-      [buttonName]: !prevState[buttonName],
-    }));
+    setSelectedOption((prevSelected) =>
+      prevSelected === buttonName ? null : buttonName
+    );
   };
-  return (
 
+  return (
     <div className="fondo relative isolate overflow-hidden bg-gray-900 py-24 sm:py-52">
       <div className='logo'><img src={LogoNsf} alt="Logo de página" aria-label='logo de pagina' /></div>
       <img src={portadaPortfolio}
-        // src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
         alt="imagen de fondo de pagina"
         aria-label='fondo de pagina'
         className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
@@ -57,7 +50,6 @@ export default function App() {
         className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
         aria-hidden="true"
       >
-
         <div
           className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
           style={{
@@ -76,49 +68,47 @@ export default function App() {
             Full-Stack developer</p>
         </div>
         <div className="mx-auto mt-20 max-w-2xl lg:mx-0 lg:max-w-none" id='caja'>
-        <div className="links-container">
-   {links.map((link) => (
+          <div className="links-container">
+            {links.map((link) => (
               <a key={link.name} href={link.href} target='_BLANK' rel="noopener noreferrer" tabIndex={0} className='name link'>
                 {link.name} <span aria-hidden="true">&rarr;</span>
               </a>
             ))}
-    </div>
-          <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-         
-           {stats.map((stat, index) => (
-  <div key={stat.name} className={`flex stat${index + 1}`} style={{ position: 'relative' }}>
-    <button
-      className="dropdown-trigger"
-      onClick={() => handleButtonClick(`button${index + 1}`)}
-    >
-      <dt className="textos text-base leading-8 text-gray-100" id='tituloInformacion'>{stat.name}</dt>
-      <span className="arrow-down">&#9660;</span>
-    </button>
-    <dd className="textos text-1xl leading-9 tracking-tight text-white" id='titInfo' style={{ position: 'absolute', top: '100%', left: 0, display: buttonStates[`button${index + 1}`] ? 'block' : 'none' }}>
-      <div className="info-container">{stat.value}</div>
-    </dd>
-    
-  </div>
-))}
-
           </div>
-          {/* <dl className="info mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4" id='cuadro'>
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
             {stats.map((stat, index) => (
-              <div key={stat.name} className={`flex flex-col-reverse stat${index + 1}`}>
-                <dt className="textos text-base leading-8 text-gray-100" id='info'>{stat.name}</dt>
-                <dd className="textos infor text-3xl leading-9 tracking-tight text-white" id='titInfo'>{stat.value}</dd>
+              <div
+                key={stat.name}
+                className={`flex stat${index + 1}`}
+                style={{ position: 'relative' }}
+              >
+                <button
+                  className="dropdown-trigger"
+                  onClick={() => handleButtonClick(`button${index + 1}`)}
+                >
+                  <dt className="textos text-base leading-8 text-gray-100" id='tituloInformacion'>
+                    {stat.name}
+                  </dt>
+                  <span className="arrow-down">&#9660;</span>
+                </button>
+                <dd
+                  className="textos text-1xl leading-9 tracking-tight text-white"
+                  id='titInfo'
+                  style={{
+                    position: 'absolute',
+                    top: '100%',
+                    left: 0,
+                    display:
+                      selectedOption === `button${index + 1}` ? 'block' : 'none',
+                  }}
+                >
+                  <div className="info-container">{stat.value}</div>
+                </dd>
               </div>
             ))}
-          </dl> */}
-
-
+          </div>
         </div>
-
-
-
       </div>
-
-
-
-
-
+    </div>
+  );
+}
