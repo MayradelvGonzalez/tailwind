1c84089e8a1097eefba35ec504308a6c66ab2cb2
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-03-05 16:05:43 -0300

    borrandoElementos

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,8 +2,7 @@ import './Footer.css';
 function Footer(){
     return(
         <div className='foot'><h3>Portfolio Mayra 💖 2024</h3>
-    <p>I <strong>believe</strong> that by constantly improving our skills and creating accessible
-solutions, we can make a difference in the lives of people around the world</p>
+    
        </div>
     )
 }
--- src/Form/MyForm.jsx
+++ src/Form/MyForm.jsx
@@ -1,76 +1,76 @@
-import React, { useState } from "react";
-import { TextField, Button } from "@mui/material";
-import './MyForm.css';
-import Alert from '@mui/material/Alert';
+// import React, { useState } from "react";
+// import { TextField, Button } from "@mui/material";
+// import './MyForm.css';
+// import Alert from '@mui/material/Alert';
 
 
-const MyForm = () => {
-  // Estados para manejar el mensaje y el estado de envío
-  const [message, setMessage] = useState('');
-  const [isSubmitted, setSubmitted] = useState(false);
+// const MyForm = () => {
+//   // Estados para manejar el mensaje y el estado de envío
+//   const [message, setMessage] = useState('');
+//   const [isSubmitted, setSubmitted] = useState(false);
 
-  // Función para manejar cambios en el campo de mensaje
-  const handleInputChange = (event) => {
-    setMessage(event.target.value);
-  };
+//   // Función para manejar cambios en el campo de mensaje
+//   const handleInputChange = (event) => {
+//     setMessage(event.target.value);
+//   };
 
-  // Función para manejar el envío del formulario
-  const handleSubmit = () => {
-    // Realizar acciones adicionales con el mensaje, como enviarlo a un servidor, etc.
-    console.log("Mensaje recibido:", message);
+//   // Función para manejar el envío del formulario
+//   const handleSubmit = () => {
+//     // Realizar acciones adicionales con el mensaje, como enviarlo a un servidor, etc.
+//     console.log("Mensaje recibido:", message);
 
-    // Limpiar el campo de mensaje después de enviar y marcar como enviado
-    setMessage('');
-    setSubmitted(true);
-  };
+//     // Limpiar el campo de mensaje después de enviar y marcar como enviado
+//     setMessage('');
+//     setSubmitted(true);
+//   };
 
-  return (
-    <div id="formulario">
-      <fieldset>
-        <legend>Contact me!</legend>
-        <div className="seccion">
-          <TextField
-            label="Full name"
-            variant="outlined"
-            id="opcion"
-          />
-        </div>
-        <div className="seccion">
-          <TextField
-            label="Email"
-            variant="outlined"
-            type="email"
-            id="opcion2"
-          />
-        </div>
-        <div className="seccion">
-          <TextField
-            label="Message"
-            variant="outlined"
-            multiline
-            rows={6}
-            id="opcion3"
-            value={message}
-            onChange={handleInputChange}
-          />
-        </div>
-        <Button
-          variant="contained"
-          color="primary"
-          className="seccion"
-          id="opcion4"
-          onClick={handleSubmit}
-        >
-          Submit
-        </Button>
+//   return (
+//     <div id="formulario">
+//       <fieldset>
+//         <legend>Contact me!</legend>
+//         <div className="seccion">
+//           <TextField
+//             label="Full name"
+//             variant="outlined"
+//             id="opcion"
+//           />
+//         </div>
+//         <div className="seccion">
+//           <TextField
+//             label="Email"
+//             variant="outlined"
+//             type="email"
+//             id="opcion2"
+//           />
+//         </div>
+//         <div className="seccion">
+//           <TextField
+//             label="Message"
+//             variant="outlined"
+//             multiline
+//             rows={6}
+//             id="opcion3"
+//             value={message}
+//             onChange={handleInputChange}
+//           />
+//         </div>
+//         <Button
+//           variant="contained"
+//           color="primary"
+//           className="seccion"
+//           id="opcion4"
+//           onClick={handleSubmit}
+//         >
+//           Submit
+//         </Button>
 
-        {/* Mostrar mensaje de enviado si el formulario ha sido enviado */}
-        {isSubmitted && <Alert variant="filled" severity="success">
-  Message Send
-</Alert>}
-      </fieldset>
-    </div>
-  );
-};
+//         {/* Mostrar mensaje de enviado si el formulario ha sido enviado */}
+//         {isSubmitted && <Alert variant="filled" severity="success">
+//   Message Send
+// </Alert>}
+//       </fieldset>
+//     </div>
+//   );
+// };
 
-export default MyForm;
+// export default MyForm;
