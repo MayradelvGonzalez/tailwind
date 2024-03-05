f4c81dab8f5dd51daf210d65f6e7b1635d2995c7
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-03-05 16:04:33 -0300

    aliminandoForm

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Footer from './Footer/Footer';
-import MyForm from './Form/MyForm';
+// import MyForm from './Form/MyForm';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
-      <Route path="/contact" element={<MyForm />} /> {/* Route for MyForm */}
+      {/* <Route path="/contact" element={<MyForm />} /> Route for MyForm */}
       <Route path="/" element={<App />} /> {/* Route for App */}
     </Routes>
     <Footer /> {/* Footer outside of Routes */}
